580b0dd3e1f5f5a6988f0c9eca6b5c9b29d53d56
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-12-17 17:32:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,4 +36,11 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
+  def self.create(attributes)
+    STUDENT.new(name,grade)
+  end
+  
+  
+  
+  
 end
